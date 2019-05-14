30477689d64cf16e1d43bd4769ee5074bcc12b4c
Guergeiro <breno-salles@hotmail.com>
2019-05-14 19:28:50 +0100

    JavaDocs para V1.0 adicionado

--- src/RequisicaoCompraCopia.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-
-public class RequisicaoCompraCopia extends RequisicaoCompra {
-  // Constructor
-  public RequisicaoCompraCopia(String estado, LocalDate data, String oficio, Livro livro,
-      Utilizador utilizador) {
-    super(estado, data, oficio, livro, utilizador);
-  }
-}
